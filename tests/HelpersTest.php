8e098a6948635d1808f6cab25afcaca4fdfaeb0c
Hosmel Quintana <hosmelq@gmail.com>
2023-08-13 18:31:48 -0600

    fix import

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use HosmelQ\LogSnag\Laravel\Contracts\ClientContract;
+use function HosmelQ\LogSnag\Laravel\logsnag;
 
 describe('logsnag', function (): void {
     it('should returns an instance of ClientContract', function (): void {
